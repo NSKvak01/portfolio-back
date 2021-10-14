3f7a5aad7c3dd27e6f9a08bdbb60f0d9ea327f15
Nastya Kvak <nastya.kvak@codeimmersives.com>
2021-10-14 14:31:23 -0400

    AWS

--- routes/email/controller/emailController.js
+++ routes/email/controller/emailController.js
@@ -4,7 +4,7 @@ const smtpTransport = require("nodemailer-smtp-transport")
 CLIENT_ID="1006631961454-66m5c0tj9k8mla0jpqoa2jctsqrb54kv.apps.googleusercontent.com"
 CLIENT_SECRET="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"
 REDIRECT_URI="https://developers.google.com/oauthplayground"
-REFRESH_TOKEN="1//044kN3NAJg7BiCgYIARAAGAQSNwF-L9IrZQMBz93JOWSadPbMGdNSAbx91Dl7bllnduDNcqfHIkixErEWONSRqXR6axog0h-qaFk"
+REFRESH_TOKEN="1//049EAANrbW8XGCgYIARAAGAQSNwF-L9Irio_GNNkbv157KEngCRsCS4EsC30BfubgPOerpoBTvrLN5oCNcGBDu9VC1uoV9357lr4"
 
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
 oAuth2Client.setCredentials({refresh_token: REFRESH_TOKEN})
