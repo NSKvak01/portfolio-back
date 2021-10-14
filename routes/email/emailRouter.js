b0572a6e11eca7fd3397319d265e8353d1c4e3b0
Nastya Kvak <nastya.kvak@codeimmersives.com>
2021-10-14 15:17:27 -0400

    AWS

--- routes/email/emailRouter.js
+++ routes/email/emailRouter.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const dotenv = require('dotenv');
-const transporter = require('./controller/emailController');
+const transport = require('./controller/emailController');
 dotenv.config();
 
 
@@ -19,7 +19,7 @@ router.post('/send', (req, res) => {
         `
       };
   
-      transporter.sendMail(mailOptions, function (err, info) {
+      transport.sendMail(mailOptions, function (err, info) {
         if (err) {
           console.log(err)
           res.status(500).json({
